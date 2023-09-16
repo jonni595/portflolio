f6ed24c04b6ebff343f9a09ebab522d1a57ad817
Jonathan <jonathandajome@gmail.com>
2023-09-15 19:02:49 -0500

    add about screen step one

--- src/screens/AboutScreen.tsx
+++ src/screens/AboutScreen.tsx
@@ -1,8 +1,49 @@
+import { BiWorld } from "react-icons/bi";
 
 const AboutScreen = () => {
   return (
-    <div>AboutScreen</div>
-  )
-}
+    <>
+      <section className="about">
+        <article className="about__content">
+          <h2>Who I am?</h2>
+          <section className="beginning">
+            <h3>My Country 🗺️</h3>
+            <p>
+              Hi!👋 I'm Jonathan Dajome, a 28-year-old Colombian ☕ web
+              developer. I'm passionate about technology and design, and I'm
+              always looking for new ways to create engaging and interactive
+              digital experiences. In my free time, I enjoy playing computer
+              games, playing sports, reading, studying, thinking, watching TV
+              shows, and developing web applications. I'm a creative and
+              innovative person, and I like to work on challenging projects that
+              allow me to learn and grow. You can see some of my most recent
+              projects and my web development skills and experience in my
+              portfolio. If you're looking for a creative and talented web
+              developer, please don't hesitate to contact me.
+            </p>
+          </section>
+          <section className="education">
+            <h3>Education</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium quod sequi numquam qui aliquid sapiente. Voluptas iste
+              obcaecati ipsa nam enim itaque amet, culpa quae esse incidunt
+              nihil tenetur? Iusto!
+            </p>
+          </section>
+          <section className="profession">
+            <h3>Profession</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium quod sequi numquam qui aliquid sapiente. Voluptas iste
+              obcaecati ipsa nam enim itaque amet, culpa quae esse incidunt
+              nihil tenetur? Iusto!
+            </p>
+          </section>
+        </article>
+      </section>
+    </>
+  );
+};
 
-export { AboutScreen }
+export { AboutScreen };
