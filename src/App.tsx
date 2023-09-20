25e26e870be7f3ebe3d9f7d04c5d886610161211
Jonathan <jonathandajome@gmail.com>
2023-09-20 08:51:47 -0500

    add scroll top component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import { IoArrowUp } from 'react-icons/io5';
+import ScrollToTop from "react-scroll-to-top";
 import { AboutScreen, ContactScreen, HomeScreen } from "./screens";
 import { Footer, Header } from "./components";
 
@@ -6,6 +8,7 @@ const App = () => {
   return (
     <>
       <Header />
+      <ScrollToTop smooth top={450} component={<IoArrowUp className="icon" />} className="scroll-Top" />
       <main>
         <Routes>
           <Route index path="/" element={<HomeScreen />} />
