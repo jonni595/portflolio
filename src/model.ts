230b6a76dbd252ced5162832b3ca97e8112e07b2
Jonathan <jonathandajome@gmail.com>
2023-09-20 11:50:03 -0500

    add form data

---
+++ src/model.ts
@@ -0,0 +1,13 @@
+interface Props {
+	firstName: string;
+	email: string;
+	message: string;
+}
+
+export const initialState: Props = {
+	firstName: "",
+	email: "",
+	message: ""
+}
+
+export const USER_EMAIL = "jonathandajome@gmail.com"
--- src/screens/ContactScreen.tsx
+++ src/screens/ContactScreen.tsx
@@ -1,11 +1,42 @@
+import { useState } from "react";
+import { initialState, USER_EMAIL } from "../model";
+
 const ContactScreen = () => {
+  const [person, setPerson] = useState(initialState);
+
+  const handleChangeInput = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setPerson({
+      ...person,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    /* if (
+      person.firstName === "" ||
+      person.email === "" ||
+      person.message === ""
+    ) {
+      alert("Please complete all fields");
+    } else {
+      window.location.href = `mailto:${USER_EMAIL}`;
+      setPerson(initialState);
+    } */
+    setPerson(initialState);
+  };
+
   return (
     <div className="contact">
       <div className="contact__container">
-        <form>
+        <form action={`mailto:${USER_EMAIL}`} method="post" encType="text/plain" >
           <label htmlFor="name">Name</label>
           <input
-          required
+            onChange={handleChangeInput}
+            value={person.firstName}
+            required
             placeholder="Jane Doe"
             type="text"
             name="firstName"
@@ -14,7 +45,9 @@ const ContactScreen = () => {
           />
           <label htmlFor="email">Email</label>
           <input
-          required
+            onChange={handleChangeInput}
+            value={person.email}
+            required
             placeholder="email@website.com"
             type="email"
             name="email"
@@ -23,8 +56,18 @@ const ContactScreen = () => {
           />
           <label htmlFor="message">Message</label>
           <textarea
-          required placeholder="What do you like to say?" name="message" id="message" cols={30} rows={10}></textarea>
-          <button type="submit">Submit</button>
+            onChange={handleChangeInput}
+            value={person.message}
+            required
+            placeholder="What do you like to say?"
+            name="message"
+            id="message"
+            cols={30}
+            rows={10}
+          ></textarea>
+          <button type="submit" onClick={handleSubmit}>
+            Submit
+          </button>
         </form>
       </div>
     </div>
