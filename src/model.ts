b4b1977e28148725d32c243803c475c2ac239e54
Jonathan Dajome R. <jonathandajome@gmail.com>
2023-09-22 12:50:55 -0500

    update card component

--- src/model.ts
+++ src/model.ts
@@ -1,13 +1,85 @@
 interface Props {
-	firstName: string;
-	email: string;
-	message: string;
+  firstName: string;
+  email: string;
+  message: string;
 }
 
 export const initialState: Props = {
-	firstName: "",
-	email: "",
-	message: ""
+  firstName: "",
+  email: "",
+  message: "",
+};
+
+interface Card {
+  id: number;
+  title: string;
+  imageUrl: string;
+  altImg: string;
+  paragraph: string;
 }
+
+export const projects: Card[] = [
+  {
+    id: 0,
+    title: "Quiz App",
+    imageUrl: "https://picsum.photos/200",
+    altImg: "Project 1",
+    paragraph:
+      "Challenge your knowledge and show how much you know about web development",
+  },
+  {
+    id: 1,
+    title: "Todo List",
+    imageUrl: "https://picsum.photos/200",
+    altImg: "Project 2",
+    paragraph:
+      "A Todo list app made in React is a web application that allows users to create and manage a to-do list",
+  },
+  {
+    id: 2,
+    title: "Movie App",
+    imageUrl: "https://picsum.photos/200",
+    altImg: "Project 3",
+    paragraph:
+      "An application for film lovers who want to find information about movies",
+  },
+  {
+    id: 3,
+    title: "Calculator",
+    imageUrl: "https://picsum.photos/200",
+    altImg: "Project 4",
+    paragraph:
+      "Web application that allows you to perform basic mathematical operations",
+  },
+  {
+    id: 4,
+    title: "Validation Form",
+    imageUrl: "https://picsum.photos/200",
+    altImg: "Project 5",
+    paragraph:
+      "It is a form that verifies that the data entered by the user is correct",
+  },
+  {
+    id: 5,
+    title: "YouTube Clone",
+    imageUrl: "https://picsum.photos/200",
+    altImg: "Project 6",
+    paragraph: "A replica of the most popular video platform on the Internet",
+  },
+];
+
+interface BadgeProps {
+  id: number;
+  langs: string[];
+}
+
+export const badges: BadgeProps[] = [
+  { id: 1, langs: ["HTML", "CSS", "Vanilla JS"] },
+  { id: 2, langs: ["HTML", "CSS", "JavaScript"] },
+  { id: 3, langs: ["Vanilla JS", "API REST", "CSS"] },
+  { id: 4, langs: ["HTML", "SASS", "CSS"] },
+  { id: 5, langs: ["HTML", "Vanilla JS", "SASS"] },
+  { id: 6, langs: ["HTML", "SASS", "CSS"] },
+];
 
 // export const USER_EMAIL = "jonathandajome@gmail.com"
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,5 @@
 import { Avatar, Badge, Button, Card } from "../components";
-import { imageUrl } from "../utils/ImageUrl";
-
-const { url1, url2, url3 } = imageUrl;
+import { projects } from "../model";
 
 const HomeScreen = () => {
   return (
@@ -17,55 +15,17 @@ const HomeScreen = () => {
         </div>
       </section>
       <section id="project" className="project">
-        <Card
-          url={url1}
-          altImage="Project 1"
-          title="Quiz App"
-          description="Challenge your knowledge and show how much you know about web development"
-        >
-          <Badge lang1="HTML" lang2="CSS" lang3="Vanilla JS" />
-        </Card>
-        <Card
-          url={url2}
-          altImage="Project 2"
-          title="Todo List"
-          description="A Todo list app made in React is a web application that allows users to create and manage a to-do list"
-        >
-          <Badge lang1="HTML" lang2="CSS" lang3="JavaScript" />
-        </Card>
-
-        <Card
-          url={url3}
-          altImage="Project 3"
-          title="Movie App"
-          description="An application for film lovers who want to find information about movies"
-        >
-          <Badge lang1="Vanilla JS" lang2="API REST" lang3="CSS" />
-        </Card>
-        <Card
-          url={url3}
-          altImage="Project 4"
-          title="Calculator"
-          description="Web application that allows you to perform basic mathematical operations"
-        >
-          <Badge lang1="HTML" lang2="SASS" lang3="CSS" />
-        </Card>
-        <Card
-          url={url3}
-          altImage="Project 5"
-          title="Validation Form"
-          description="It is a form that verifies that the data entered by the user is correct"
-        >
-          <Badge lang1="HTML" lang2="Vanilla JS" lang3="SASS" />
-        </Card>
-        <Card
-          url={url3}
-          altImage="Project 6"
-          title="YouTube Clone"
-          description="A replica of the most popular video platform on the Internet"
-        >
-          <Badge lang1="HTML" lang2="SASS" lang3="CSS" />
-        </Card>
+        {projects.map((project) => (
+          <Card
+            key={project.id}
+            title={project.title}
+            description={project.paragraph}
+            url={project.imageUrl}
+            altImage={project.altImg}
+          >
+            <Badge lang1="HTML" lang2="CSS" lang3="Vanilla JS" />
+          </Card>
+        ))}
       </section>
     </>
   );
