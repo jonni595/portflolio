130da6372cd080fbd8b7386a29b2abcbeebccdc5
Jonathan <jonathandajome@gmail.com>
2023-10-01 00:15:30 -0500

    react-simple-typewriter library added

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,23 @@
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 import { Avatar, Badge, Button, Card } from "../components";
 import { getLimit } from "../utils/getLimit";
 import { projects } from "../utils/model";
 
 const HomeScreen = () => {
+  const [text] = useTypewriter({
+    words: ["Frontend Developer", "I 💛 JavaScript"],
+    loop: 0,
+    typeSpeed: 80,
+    deleteSpeed: 50,
+  });
   return (
     <>
       <section className="profile">
         <div className="profile__item">
           <Avatar />
-          <h1 className="profile__title">Frontend Developer</h1>
+          <h1 className="profile__title">
+            {text} <Cursor />
+          </h1>
           <span className="profile__subtitle">
             Freelancer passionate about development
           </span>
@@ -16,18 +25,28 @@ const HomeScreen = () => {
         </div>
       </section>
       <section id="project" className="project">
-        {projects.map(({id, title, paragraph, imageUrl, altImg, projectUrl, languages}) => (
-          <Card
-            key={id}
-            title={title}
-            description={`${getLimit(paragraph)}`}
-            url={imageUrl}
-            altImage={altImg}
-            gitHubUrl={projectUrl}
-          >
-            <Badge langs={languages} />
-          </Card>
-        ))}
+        {projects.map(
+          ({
+            id,
+            title,
+            paragraph,
+            imageUrl,
+            altImg,
+            projectUrl,
+            languages,
+          }) => (
+            <Card
+              key={id}
+              title={title}
+              description={`${getLimit(paragraph)}`}
+              url={imageUrl}
+              altImage={altImg}
+              gitHubUrl={projectUrl}
+            >
+              <Badge langs={languages} />
+            </Card>
+          )
+        )}
       </section>
     </>
   );
