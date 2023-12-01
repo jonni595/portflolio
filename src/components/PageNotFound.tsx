706ce6c783c1109711e66313b80a5e669c3ce1d7
Jonathan <jonathandajome@gmail.com>
2023-12-01 14:41:39 -0500

    add PageNotFound component

---
+++ src/components/PageNotFound.tsx
@@ -0,0 +1,12 @@
+const PageNotFound = () => {
+  return (
+    <div className="page-not-found">
+      <div className="background-wrapper">
+        <h1>404</h1>
+        <p>The page you're looking for does not exist.</p>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
