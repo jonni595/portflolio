71a0a2fc533b96dc0a67e7317499813ca9c9211a
Jonathan <jonathandajome@gmail.com>
2024-11-26 09:03:31 -0500

    add new project

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,35 @@
-import { Routes, Route } from "react-router-dom";
-import { IoArrowUp } from "react-icons/io5";
-import ScrollToTop from "react-scroll-to-top";
-import { AboutScreen, ContactScreen, HomeScreen } from "./screens";
-import { Footer, Header } from "./components";
-import PageNotFound from "./components/PageNotFound";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <Header />
-      <ScrollToTop
-        smooth
-        top={450}
-        component={<IoArrowUp className="icon" />}
-        style={{ borderRadius: "50%" }}
-      />
-      <main>
-        <Routes>
-          <Route index path="/" element={<HomeScreen />} />
-          <Route path="about" element={<AboutScreen />} />
-          <Route path="contact" element={<ContactScreen />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </main>
-      <Footer />
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
