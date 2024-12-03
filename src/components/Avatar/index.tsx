6e390c7b0167f4846b737d4ca6fba89b6ec9f23b
Jonathan <jonathandajome@gmail.com>
2024-12-03 17:56:55 -0500

    update avatar section style

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,15 +1,24 @@
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 import logo from "../../assets/avatar.png";
 import "./Avatar.css";
 
 const Avatar = () => {
+  const [text] = useTypewriter({
+    words: ["Frontend", "Freelancer", "Developer"],
+    loop: 0,
+    typeSpeed: 80,
+    deleteSpeed: 50,
+  });
   return (
     <div className="avatar">
       <div className="avatar-image">
         <img src={logo} alt="avatar" />
       </div>
       <div className="avatar-profile">
-        <h1>Jonathan Dajome</h1>
-        <p>Frontend Developer</p>
+        <h1 className="avatar-name">Jonathan D.</h1>
+        <p className="avatar-job">
+          I'm <span className="avatar-job-text">{text}</span> <Cursor />
+        </p>
         <div className="avatar-social">
           <a className="button" href="#">
             Github
