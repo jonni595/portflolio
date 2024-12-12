6100773e45449f1ed30361480bae304f29b94218
Jonathan <jonathandajome@gmail.com>
2024-12-11 21:54:22 -0500

    update experience styles

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -13,64 +13,59 @@ import "react-vertical-timeline-component/style.min.css";
 const Timeline = () => {
   return (
     <>
-      <VerticalTimeline lineColor="#00214d">
+      <VerticalTimeline lineColor="#282a36">
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          contentStyle={{ background: "#f2f4f6", color: "#1b2d45" }}
-          contentArrowStyle={{ borderRight: "7px solid  #f2f4f6" }}
+          contentStyle={{ background: "#282a36", color: "#fffffe" }}
+          contentArrowStyle={{ borderRight: "7px solid  #282a36" }}
           date="2023 - Present"
-          iconStyle={{ background: "#00214d", color: "#ffffff" }}
+          iconStyle={{ background: "#282a36", color: "#fffffe" }}
           icon={<IoLogoReact />}
         >
           <h3 className="vertical-timeline-element-title">
             Frontend Developer
           </h3>
-          <h4 className="vertical-timeline-element-subtitle">Freelance</h4>
-          <p>Build and maintain web applications</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: "#f2f4f6", color: "#1b2d45" }}
-          contentArrowStyle={{ borderRight: "7px solid  #f2f4f6" }}
-          date="2021 - 2023"
-          iconStyle={{ background: "#00214d", color: "#ffffff" }}
-          icon={<IoLaptopOutline />}
-        >
-          <h3 className="vertical-timeline-element-title">Web Developer</h3>
-          <h4 className="vertical-timeline-element-subtitle">jr developer</h4>
-          <p>Develop and maintain web applications</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: "#f2f4f6", color: "#1b2d45" }}
-          contentArrowStyle={{ borderRight: "7px solid  #f2f4f6" }}
-          date="2019 - 2021"
-          iconStyle={{ background: "#00214d", color: "#ffffff" }}
-          icon={<IoBriefcaseOutline />}
-        >
-          <h3 className="vertical-timeline-element-title">Community Manager</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            Customer Service Executive
-          </h4>
           <p>
-            Provide customer service to the community by providing support and
-            resolving issues
+            Design and implementation of the navigation structure between
+            screens with react native
           </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          contentStyle={{ background: "#f2f4f6", color: "#1b2d45" }}
-          contentArrowStyle={{ borderRight: "7px solid  #f2f4f6" }}
+          contentStyle={{ background: "#282a36", color: "#fffffe" }}
+          contentArrowStyle={{ borderRight: "7px solid  #282a36" }}
+          date="2021 - 2022"
+          iconStyle={{ background: "#282a36", color: "#fffffe" }}
+          icon={<IoLaptopOutline />}
+        >
+          <h3 className="vertical-timeline-element-title">Web Developer</h3>
+          <p>Develop and maintain web applications</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "#282a36", color: "#fffffe" }}
+          contentArrowStyle={{ borderRight: "7px solid  #282a36" }}
+          date="2019 - 2021"
+          iconStyle={{ background: "#282a36", color: "#fffffe" }}
+          icon={<IoBriefcaseOutline />}
+        >
+          <h3 className="vertical-timeline-element-title">Community Manager</h3>
+          <p>
+            Provided customer service through social media providing effective
+            responses and solutions to queries and requirements
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "#282a36", color: "#fffffe" }}
+          contentArrowStyle={{ borderRight: "7px solid  #282a36" }}
           date="2017 - 2019"
-          iconStyle={{ background: "#00214d", color: "#ffffff" }}
+          iconStyle={{ background: "#282a36", color: "#fffffe" }}
           icon={<SlEarphones />}
         >
           <h3 className="vertical-timeline-element-title">
             Customer Service Executive
           </h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            Customer Service Executive
-          </h4>
           <p>
             Provide customer service to the community by providing support and
             resolving issues
