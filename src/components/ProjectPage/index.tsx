f3cf9f13048031a3111b4b30459d45c4c201523c
Jonathan <jonathandajome@gmail.com>
2024-12-26 09:27:34 -0500

    Remove skillsFilter

--- src/components/ProjectPage/index.tsx
+++ src/components/ProjectPage/index.tsx
@@ -1,6 +1,5 @@
 import { projects } from "../../Utils/data";
 import Card from "../Card";
-import SkillsFilter from "../SkillsFilter";
 import "./Project.css";
 
 const Project = () => {
@@ -8,7 +7,6 @@ const Project = () => {
     <div className="container" id="projects">
       <div className="wrapper">
         <h1>Projects</h1>
-        <SkillsFilter onSelectSkill={() => {}} />
         <div className="projects">
           {projects.map((project) => (
             <Card
